83f52d8f403286bfaa59c6fbada70298b871d552
Anish100x_S <128980424+anisoni12@users.noreply.github.com>
2025-08-08 23:20:01 +0530

    Time: 1 ms (84.36%) | Memory: 41 MB (40.75%) - LeetSync

---
+++ StackQueue/69-sqrtx/sqrtx.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x < 2) return x;
+        int l = 1, r = x/2;
+        while(l <= r) {
+            int mid = l + (r-l) / 2;
+            long sq = (long) mid * mid;
+            if(sq == x) return mid;
+            if(sq < x) l = mid + 1;
+            else r = mid - 1; 
+        }
+        return r ;
+    }
+}
